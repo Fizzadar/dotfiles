ada8494d673a5577d8ddd89ee780eb51277ae2cb
Nick Barrett <pointlessrambler@gmail.com>
2016-01-16 11:02:21 +0000

    Sort out update/bootstrap.

---
+++ bootstrap.sh
@@ -0,0 +1,24 @@
+# Fizzadar's .dotfiles
+# File: bootstrap.sh
+# Desc: setup .dotfiles
+
+set -e
+
+echo "### Bootstrapping Fizzadar's .dotfiles..."
+echo
+
+
+# Check for existing dotfiles
+if [ -d "${HOME}/.dotfiles" ]; then
+    echo "--> There already appears to be a ~/.dotfiles, exiting!"
+    exit 1
+fi
+
+# Make the .dotfiles dir
+mkdir -p "${HOME}/.dotfiles"
+
+# Clone it
+git clone git@github.com:Fizzadar/dotfiles.git "${HOME}/.dotfiles"
+
+# Update/install it
+dotupdate
